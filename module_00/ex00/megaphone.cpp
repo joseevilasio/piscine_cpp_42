5c6cedb47a6686d4524ab6e22f5cb71c4cde241c
Jose Junior <jose_evilasio@outlook.com>
2025-04-15 18:17:21 +0100

    initial setup

---
+++ module_00/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+std::string	toUpper(std::string str)
+{
+	for (size_t i = 0; i < str.size(); i++)
+	{
+		str[i] = std::toupper(str[i]);
+	}
+	return (str);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::cout << toUpper(argv[i]);
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << "\n";
+	return (0);
+}
