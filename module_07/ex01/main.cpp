164fa0f0454f0f81f6403b5ea56964cc110ef0c2
Jose Junior <jose_evilasio@outlook.com>
2025-05-30 19:56:47 +0100

    fixed main

--- module_07/ex01/main.cpp
+++ module_07/ex01/main.cpp
@@ -2,7 +2,39 @@
 
 int main(void)
 {
-	int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	iter(&arr, 10, &ft_print);
+	int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	iter(arr, 10, ft_print<int>);
 	return (0);
 }
+
+// class Awesome
+// {
+// 	public:
+// 		Awesome( void ) : _n( 42 ) { return; }
+// 		int get( void ) const { return this->_n; }
+// 	private:
+// 		int _n;
+// };
+
+// std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+// {
+// 	o << rhs.get();
+// 	return o;
+// }
+
+// template< typename T >
+// void print( T& x )
+// {
+// 	std::cout << x << std::endl;
+// 	return;
+// }
+
+// int main() {
+// 	const int tab[] = { 0, 1, 2, 3, 4 };
+// 	Awesome tab2[5];
+
+// 	iter( tab, 5, print<const int> );
+// 	iter( tab2, 5, print<Awesome> );
+
+// 	return 0;
+// }
