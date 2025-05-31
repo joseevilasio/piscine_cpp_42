66ff26ae014c29037c918a01a6d222a87296b96b
Jose Junior <jose_evilasio@outlook.com>
2025-05-31 17:52:05 +0100

    fixed double free and logic

--- module_07/ex02/Array.hpp
+++ module_07/ex02/Array.hpp
@@ -18,12 +18,13 @@ class Array
 		Array(const Array& rhs);
 		~Array(void);
 
-		unsigned int	size(void);
+		unsigned int	size(void) const;
+		const T&	operator[](unsigned int i) const;
 		T&	operator[](unsigned int i);
 };
 
 template <typename T>
-Array<T>::Array(void) : _elements(new T()), _size(0) {}
+Array<T>::Array(void) : _elements(NULL), _size(0) {}
 
 template <typename T>
 Array<T>::Array(unsigned int n) : _elements(new T[n]), _size(n) {}
@@ -33,28 +34,55 @@ Array<T>& Array<T>::operator=(const Array<T>& rhs)
 {
 	if (this != &rhs)
 	{
-		_elements = rhs._elements;
+		delete[] _elements;
+		_elements = NULL;
+		_size = 0;
+		if (rhs._size > 0)
+		{
+			_elements = new T[rhs._size];
+			for(unsigned int i = 0; i < rhs._size; i++)
+			{
+				_elements[i] = rhs._elements[i];
+			}
+			_size = rhs._size;
+		}
 	}
 	return (*this);
 }
 
 template <typename T>
-Array<T>::Array(const Array<T>& rhs)
+Array<T>::Array(const Array<T>& rhs) : _elements(NULL), _size(0)
 {
-	this = rhs;
+	if (rhs._size > 0)
+	{
+		_elements = new T[rhs._size];
+		for(unsigned int i = 0; i < rhs._size; i++)
+		{
+			_elements[i] = rhs._elements[i];
+		}
+		_size = rhs._size;
+	}
 }
 
 template <typename T>
 Array<T>::~Array(void) {delete[] _elements;}
 
 template <typename T>
-unsigned int	Array<T>::size(void) {return (_size);}
+unsigned int	Array<T>::size(void) const {return (_size);}
+
+template <typename T>
+const T&	Array<T>::operator[](unsigned int i) const
+{
+	if (i >= _size)
+		throw std::out_of_range("Outside the interval");
+	return (_elements[i]);
+}
 
 template <typename T>
 T&	Array<T>::operator[](unsigned int i)
 {
-	if (i > _size)
-		throw std::out_of_range::out_of_range();
+	if (i >= _size)
+		throw std::out_of_range("Outside the interval");
 	return (_elements[i]);
 }
 
