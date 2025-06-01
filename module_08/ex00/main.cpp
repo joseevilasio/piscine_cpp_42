8134eecb9c7d755cc0ec4895513672929b9355b5
Jose Junior <jose_evilasio@outlook.com>
2025-06-01 17:33:05 +0100

    my tests

--- module_08/ex00/main.cpp
+++ module_08/ex00/main.cpp
@@ -1,6 +1,48 @@
 #include <easyfind.hpp>
+#include <vector>
+#include <list>
+#include <iostream>
 
 int main(void)
 {
+	{
+		std::cout << "\n TEST 01 - Vector\n";
+
+		std::vector<int> v;
+	
+		v.push_back(42);
+		v.push_back(22);
+		v.push_back(12);
+		v.push_back(2);
+	
+		try
+		{
+			std::cout << "Value: " << easyfind(v, 42) << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
+
+	{
+		std::cout << "\n TEST 02 - List\n";
+		
+		std::list<int> l;
+	
+		l.push_back(42);
+		l.push_back(22);
+		l.push_back(12);
+		l.push_back(2);
+	
+		try
+		{
+			std::cout << "Value: " << easyfind(l, 12) << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
 	return (0);
 }
