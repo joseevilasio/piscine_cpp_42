08d1b8a7976d2e80b51197824b54c85447a99e8c
Jose Junior <jose_evilasio@outlook.com>
2025-06-02 20:54:55 +0100

    impl

---
+++ module_08/ex01/Span.cpp
@@ -0,0 +1,13 @@
+#include "Span.hpp"
+
+Span::Span(void) {}
+
+Span::Span(unsigned int n) : _max(n) {}
+
+Span& Span::operator=(const Span& rhs)
+
+Span::Span(const Span& rhs) {}
+
+Span::~Span(void) {}
+
+void	Span::addNumber(int nbr) {}
