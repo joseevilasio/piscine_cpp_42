b1ef9ce2bb3885d1133dfa44b16791a080887caf
Jose Junior <jose_evilasio@outlook.com>
2025-06-03 22:35:31 +0100

    added eg

--- module_06/ex00/main.cpp
+++ module_06/ex00/main.cpp
@@ -1,16 +1,62 @@
 #include "ScalarConvert.hpp"
+#include <cstdlib>
 
-int	main(int argc, char** argv)
+// int	main(int argc, char** argv)
+// {
+// 	if (argc != 2)
+// 		return (1);
+// 	try
+// 	{
+// 		ScalarConvert::convert(argv[1]);
+// 	}
+// 	catch(const std::exception& e)
+// 	{
+// 		std::cout << e.what() << '\n';
+// 	}	
+// 	return (0);
+// }
+
+int main(void)
 {
-	if (argc != 2)
-		return (1);
-	try
 	{
-		ScalarConvert::convert(argv[1]);
+		int a = 42;
+		double b = a; //implicit type conversion
+	
+		std::cout << "a: " << a << std::endl;
+		std::cout << "b: " << b << std::endl;
 	}
-	catch(const std::exception& e)
+
 	{
-		std::cout << e.what() << '\n';
-	}	
+		int a = 10;
+		double b = (double) a;
+	
+		std::cout << "a: " << a << std::endl;
+		std::cout << "b: " << b << std::endl;
+	}
+
+	{
+		double a = 100.2;
+		int b = a;
+	
+		std::cout << "a: " << a << std::endl;
+		std::cout << "b: " << b << std::endl;
+	}
+
+	{
+		std::string a = "100";
+		int b = (int) std::atoi(a.c_str());
+	
+		std::cout << "a: " << a << std::endl;
+		std::cout << "b: " << b << std::endl;
+	}
+
+	{
+		double a = 99999999999.9;
+		int b = static_cast<int>(a);
+	
+		std::cout << "a: " << a << std::endl;
+		std::cout << "b: " << b << std::endl;
+	}
+
 	return (0);
 }
