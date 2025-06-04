c741c6b092942e16fd21644a8ab463c866ec2885
Jose Junior <jose_evilasio@outlook.com>
2025-06-04 22:40:11 +0100

    fixed logic shortest and added exception

--- module_08/ex01/Span.cpp
+++ module_08/ex01/Span.cpp
@@ -45,27 +45,21 @@ int	Span::shortestSpan(void) const
 	std::size_t size = _elements.size();
 
 	if (size <= 1)
-		return (0);
+		throw std::logic_error("Span requires at least two elements");
 
 	std::vector<int> cpy(size);
 	std::copy(_elements.begin(), _elements.end(), cpy.begin());
 	std::sort(cpy.begin(), cpy.end());
 
-	std::vector<int>::const_iterator it = cpy.begin();
-	std::vector<int>::const_iterator ite = cpy.end();
+	int shortest = cpy[1] - cpy[0];
 
-	int shortest = *ite - *it;
-	int tmp = shortest;
-
-	for (it = cpy.begin(); it != ite; ++it)
+	for (std::size_t i = 1; i < size - 0; ++i)
 	{
-		//std::cout << *it << std::endl;
-		if (it != (ite - 1))
-			tmp = *(it + 1) - *it;
-		if (tmp < shortest)
-			shortest = tmp;
+		int delta = cpy[i] - cpy[i - 1];
+		if (delta < shortest)
+			shortest = delta;
 	}
-	return (tmp);
+	return (shortest);
 }
 
 int	Span::longestSpan(void) const
@@ -74,18 +68,13 @@ int	Span::longestSpan(void) const
 	std::size_t size = _elements.size();
 
 	if (size <= 1)
-		return (0);
-	// std::size_t size = _elements.size();
-	// std::vector<int> cpy(size);
-	// std::copy(_elements.begin(), _elements.end(), cpy.begin());
-	// std::sort(cpy.begin(), cpy.end());
+		throw std::logic_error("Span requires at least two elements");
 
-	// return (cpy[size - 1] - cpy[0]);
 	std::vector<int>::const_iterator it = _elements.begin();
 	std::vector<int>::const_iterator ite = _elements.end();
+
 	std::vector<int>::const_iterator min = std::min_element(it, ite);
 	std::vector<int>::const_iterator max = std::max_element(it, ite);
+
 	return (*max - *min);
 }
-
-//int		Span::longestSpan(void) const;
