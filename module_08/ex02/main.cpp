da9ad9815c9c59d278dcd8ab79293355568b8653
Jose Junior <jose_evilasio@outlook.com>
2025-06-06 19:04:23 +0100

    added tests

--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -1,39 +1,77 @@
 #include "MutantStack.hpp"
+#include <list>
 
-int main(void)
+// int main(void)
+// {
+// 	MutantStack<int>	mstack;
+
+// 	mstack.push(5);
+// 	mstack.push(17);
+
+// 	std::cout << mstack.top() << std::endl;
+
+// 	mstack.pop();
+
+// 	std::cout << mstack.size() << std::endl;
+
+// 	mstack.push(3);
+// 	mstack.push(5);
+// 	mstack.push(737);
+// 	mstack.push(7);
+// 	mstack.push(77);
+// 	mstack.push(70);
+// 	mstack.push(1);
+
+// 	MutantStack<int>::iterator it = mstack.begin();
+// 	MutantStack<int>::iterator ite = mstack.end();
+
+// 	++it;
+// 	--it;
+
+// 	while (it != ite)
+// 	{
+// 		std::cout << *it << std::endl;
+// 		++it;
+// 	}
+
+// 	std::stack<int> s(mstack);
+
+// 	return (0);
+// }
+
+int	main(void)
 {
-	MutantStack<int>	mstack;
+		MutantStack<int>	mstack;
+		std::list<int>		list;
 
-	mstack.push(5);
-	mstack.push(17);
+		mstack.push(10);
+		mstack.push(20);
+		mstack.push(30);
+		mstack.push(40);
+		mstack.push(50);
 
-	std::cout << mstack.top() << std::endl;
 
-	mstack.pop();
+		list.push_back(10);
+		list.push_back(20);
+		list.push_back(30);
+		list.push_back(40);
+		list.push_back(50);
 
-	std::cout << mstack.size() << std::endl;
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
 
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	mstack.push(7);
-	mstack.push(77);
-	mstack.push(70);
-	mstack.push(1);
+		std::list<int>::iterator l_it = list.begin();
+		//std::list<int>::iterator l_ite = list.end();
 
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
+		mstack.pop();
+		list.pop_back();
 
-	++it;
-	--it;
-
-	while (it != ite)
-	{
-		std::cout << *it << std::endl;
-		++it;
-	}
-
-	std::stack<int> s(mstack);
-
-	return (0);
+		while (it != ite)
+		{
+			std::cout << "mstack: " << *it << std::endl;
+			std::cout << "list  : " << *l_it << std::endl;
+			++it;
+			++l_it;
+		}
+		return (0);
 }
